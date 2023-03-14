93b80bb7800ece26b041b53f9391102058545161
Bohdan Makarenko <bohdan.makarenko@blackbird.io>
2023-03-14 19:10:36 +0200

    Add project files.

---
+++ Apps.Contentful/Actions.cs
@@ -0,0 +1,34 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Apps.Contentful.Models.Requests;
+using Apps.Contentful.Models.Responses;
+using Apps.Contentful.Dtos;
+using Contentful.Core.Configuration;
+using Contentful.Core;
+
+namespace Apps.Contentful
+{
+    [ActionList]
+    public class Actions
+    {
+        [Action("Get content", Description = "Get content")]
+        public GetContentResponse GetContent(AuthenticationCredentialsProvider authenticationCredentialsProvider, 
+            [ActionParameter] GetContentRequest input)
+        {
+            return new GetContentResponse();
+        }
+
+        private ContentfulClient GetContentfulClient(string deliveryApiKey, string previewApiKey, string spaceId)
+        {
+            var httpClient = new HttpClient();
+            var options = new ContentfulOptions
+            {
+                DeliveryApiKey = deliveryApiKey,
+                PreviewApiKey = previewApiKey,
+                SpaceId = spaceId
+            };
+            var client = new ContentfulClient(httpClient, options);
+            return client;
+        }
+    }
+}
---
+++ Apps.Contentful/Models/Requests/GetContentRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Contentful.Models.Requests
+{
+    public class GetContentRequest
+    {
+        public string UserLogin { get; set; }
+    }
+}
