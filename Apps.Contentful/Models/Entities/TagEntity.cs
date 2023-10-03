cec7ac1b6e85d92c499bcadf582bb9fe90c05dbb
bZverok <bohdan.zverok@blackbird.io>
2023-10-03 15:47:49 +0300

    added tag version in the output

--- Apps.Contentful/Models/Entities/TagEntity.cs
+++ Apps.Contentful/Models/Entities/TagEntity.cs
@@ -5,18 +5,19 @@ namespace Apps.Contentful.Models.Entities;
 
 public class TagEntity
 {
-    [Display("Tag ID")]
-    public string Id { get; set; }
+    [Display("Tag ID")] public string Id { get; set; }
 
     public string Name { get; set; }
 
-    [Display("Created at")]
-    public DateTime? CreatedAt { get; set; }
+    [Display("Created at")] public DateTime? CreatedAt { get; set; }
+
+    public int? Version { get; set; }
 
     public TagEntity(ContentTag tag)
     {
         Id = tag.SystemProperties.Id;
         Name = tag.Name;
         CreatedAt = tag.SystemProperties.CreatedAt;
+        Version = tag.SystemProperties.Version;
     }
 }
