e17e7ab7d4fa17f6ac2d5d85b0c4b82cffb664a9
bZverok <bohdan.zverok@blackbird.io>
2023-10-03 15:01:38 +0300

    added connection validator

---
+++ Apps.Contentful/Connections/ConnectionValidator.cs
@@ -0,0 +1,30 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.Contentful.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var client = new ContentfulClient(authProviders);
+            await client.GetContentTypes(cancellationToken: cancellationToken);
+
+            return new()
+            {
+                IsValid = true
+            };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
+    }
+}
