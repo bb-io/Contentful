c1a5580571c7b6aeb12e6f80b0a648b515a0331b
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-11-09 19:49:30 +0200

    Auth updated

--- Apps.Contentful/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.Contentful/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -1,17 +1,23 @@
-﻿using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Invocation;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace Apps.Contentful.Auth.OAuth2;
 
-public class OAuth2AuthorizeService : IOAuth2AuthorizeService
+public class OAuth2AuthorizeService : BaseInvocable, IOAuth2AuthorizeService
 {
+    public OAuth2AuthorizeService(InvocationContext invocationContext) : base(invocationContext)
+    {
+    }
+
     public string GetAuthorizationUrl(Dictionary<string, string> values)
     {
         const string oauthUrl = "https://be.contentful.com/oauth/authorize";
         var parameters = new Dictionary<string, string>
         {
             { "client_id", values["client_id"] },
-            { "redirect_uri", ApplicationConstants.RedirectUri },
+            { "redirect_uri", InvocationContext.UriInfo.ImplicitGrantRedirectUri.ToString() },
             { "response_type", "token" },
             { "scope", ApplicationConstants.Scope },
             { "state", values["state"] }
