1312f02bc4a8022156f7b80d1405c4718910a891
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-01-30 17:30:29 +0100

    Added our own models

---
+++ Apps.Contentful/Dtos/Raw/ContentTypeItem.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+namespace Apps.Contentful.Dtos.Raw
+{
+    public class ContentTypeItem
+    {
+        [JsonProperty("sys", NullValueHandling = NullValueHandling.Ignore)]
+        public SystemProperties SystemProperties { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DisplayField { get; set; }
+
+        [JsonProperty("fields", NullValueHandling = NullValueHandling.Ignore)]
+        public List<ContentTypeField>? Fields { get; set; }
+    }
+
+    public class SystemProperties
+    {
+        public string Id { get; set; }
+    }
+
+    public class ContentTypeField
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public bool Required { get; set; }
+
+        public bool Localized { get; set; }
+
+        public string LinkType { get; set; }
+
+        public bool Disabled { get; set; }
+
+        public bool Omitted { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public ContentTypeFieldSchema? Items { get; set; }
+    }
+
+    public class ContentTypeFieldSchema
+    {
+        public string LinkType { get; set; }
+    }
+}
