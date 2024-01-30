7ce184133097f5a5b4dbc83fd3b04662eaa88249
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-01-30 16:58:15 +0100

    Content models using REST API

---
+++ Apps.Contentful/Models/Wrappers/ItemWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Contentful.Models.Wrappers
+{
+    public class ItemWrapper<T>
+    {
+        public int Total { get; set; }
+        public int SKip { get; set; }
+        public int Limit { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
