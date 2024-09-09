ac4cb841fe6a4c47eef8c95ba169ee71cf7d00ce
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-09 17:06:11 +0300

    Added debug action

---
+++ Apps.Contentful/Actions/DebugActions.cs
@@ -0,0 +1,18 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+
+namespace Apps.Contentful.Actions;
+
+[ActionList]
+public class DebugActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
+    : BaseInvocable(invocationContext)
+{
+    [Action("[DEBUG] Action", Description = "Debug action")]
+    public List<AuthenticationCredentialsProvider> GetCredentialsProviders()
+    {
+        return InvocationContext.AuthenticationCredentialsProviders.ToList();
+    }
+}
