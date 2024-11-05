c47686b3a5bdeaf30a16d4f748af4ae9fdedd289
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-05 17:51:28 +0200

    bug fixes and minor improvements

---
+++ Apps.Contentful/Actions/DebugActions.cs
@@ -0,0 +1,17 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+
+namespace Apps.Contentful.Actions;
+
+[ActionList]
+public class DebugActions(InvocationContext invocationContext)
+    : BaseInvocable(invocationContext)
+{
+    [Action("Debug", Description = "Debug action")]
+    public List<AuthenticationCredentialsProvider> DebugAction()
+    {
+        return InvocationContext.AuthenticationCredentialsProviders.ToList();
+    }
+}
