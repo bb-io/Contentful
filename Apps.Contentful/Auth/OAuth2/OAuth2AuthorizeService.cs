46633ee68b460a72111ed3d7deaab71e11f9c5ee
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-08 14:46:35 +0200

    Rollback bridge OAuth changes

--- Apps.Contentful/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.Contentful/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -11,19 +11,16 @@ public class OAuth2AuthorizeService(InvocationContext invocationContext)
 {
     public string GetAuthorizationUrl(Dictionary<string, string> values)
     {        
-        var bridgeOauthUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/oauth";
         const string oauthUrl = "https://be.contentful.com/oauth/authorize";
         var parameters = new Dictionary<string, string>
         {
             { CredNames.ClientId, values["client_id"] },
-            { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/ImplicitGrant" },
-            { "actual_redirect_uri", InvocationContext.UriInfo.ImplicitGrantRedirectUri.ToString() },
-            { "authorization_url", oauthUrl},            
+            { "redirect_uri", InvocationContext.UriInfo.ImplicitGrantRedirectUri.ToString() },
             { "response_type", "token" },
             { "scope", ApplicationConstants.Scope },
             { "state", values["state"] }
         };
         
-        return QueryHelpers.AddQueryString(bridgeOauthUrl, parameters);
+        return QueryHelpers.AddQueryString(oauthUrl, parameters);
     }
 }
