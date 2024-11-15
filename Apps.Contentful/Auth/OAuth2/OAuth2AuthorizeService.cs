35ab1645397321aaf840d322b4d82a5bb10aa55b
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-15 13:25:25 +0200

    Fixed OAuth URL

--- Apps.Contentful/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.Contentful/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -2,6 +2,7 @@
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Utils.Extensions.Sdk;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace Apps.Contentful.Auth.OAuth2;
@@ -10,8 +11,8 @@ public class OAuth2AuthorizeService(InvocationContext invocationContext)
     : BaseInvocable(invocationContext), IOAuth2AuthorizeService
 {
     public string GetAuthorizationUrl(Dictionary<string, string> values)
-    {        
-        const string oauthUrl = "https://be.contentful.com/oauth/authorize";
+    {
+        var oauthUrl = GetOAuthUrl();
         var parameters = new Dictionary<string, string>
         {
             { CredNames.ClientId, values["client_id"] },
@@ -22,5 +23,18 @@ public class OAuth2AuthorizeService(InvocationContext invocationContext)
         };
         
         return QueryHelpers.AddQueryString(oauthUrl, parameters);
+    }
+
+    private string GetOAuthUrl()
+    {
+        var baseUrl = InvocationContext.AuthenticationCredentialsProviders
+            .Get(CredNames.BaseUrl).Value;
+        
+        if(baseUrl.Contains("eu"))
+        {
+            return "https://be.eu.contentful.com/oauth/authorize";
+        }
+        
+        return "https://be.contentful.com/oauth/authorize";
     }
 }
