d0731b820a69d877433fb8d2c09a2c0671c85173
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-27 11:05:24 +0200

    Removed useless class

--- Apps.Contentful/Models/Responses/WorkflowStepResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Blackbird.Applications.Sdk.Common;
-
-namespace Apps.Contentful.Models.Responses;
-
-public class WorkflowStepResponse : WorkflowResponse
-{
-    [Display("Current step name")]
-    public string CurrentStepName { get; set; } = string.Empty;
-}
