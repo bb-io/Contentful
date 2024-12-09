46417ca3fa182b62349b99e12b61abdfc2cca463
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-12-09 13:46:34 +0200

    Removed try catch blocks

--- Apps.Contentful/HtmlHelpers/EntryToJsonConverter.cs
+++ Apps.Contentful/HtmlHelpers/EntryToJsonConverter.cs
@@ -21,20 +21,13 @@ public static class EntryToJsonConverter
             .Select(x => new EntryHtmlContentDto(x.Attributes[ConvertConstants.EntryIdAttribute].Value, x))
             .ToList();
     }
-    
+
     public static void ToJson(Entry<object> entry, HtmlNode html, string locale)
     {
-        try
-        {
-            var entryFields = (JObject)entry.Fields;
+        var entryFields = (JObject)entry.Fields;
 
-            var elements = html.ChildNodes.Where(n => n.NodeType == HtmlNodeType.Element).ToList();
-            elements.ForEach(x => UpdateEntryFieldFromHtml(x, entryFields, locale));
-        }
-        catch (Exception e)
-        {
-            throw;
-        }
+        var elements = html.ChildNodes.Where(n => n.NodeType == HtmlNodeType.Element).ToList();
+        elements.ForEach(x => UpdateEntryFieldFromHtml(x, entryFields, locale));
     }
 
     private static void UpdateEntryFieldFromHtml(HtmlNode htmlNode, JObject entryFields, string locale)
@@ -42,54 +35,88 @@ public static class EntryToJsonConverter
         var fieldId = htmlNode.Attributes[ConvertConstants.FieldIdAttribute].Value;
         var fieldType = htmlNode.Attributes[ConvertConstants.FieldTypeAttribute].Value;
 
-        try
+        switch (fieldType)
         {
-            switch (fieldType)
-            {
-                case "Integer":
-                    var intValue = Convert.ToInt32(htmlNode.InnerText);
-                    entryFields[fieldId][locale] = intValue;
-                    break;
-                case "Number":
-                    var decimalValue = Convert.ToDecimal(htmlNode.InnerText);
+            case "Integer":
+                var intValue = Convert.ToInt32(htmlNode.InnerText);
+                entryFields[fieldId][locale] = intValue;
+                break;
+            case "Number":
+                var decimalValue = Convert.ToDecimal(htmlNode.InnerText);
 
-                    if (decimalValue == Decimal.Floor(decimalValue))
+                if (decimalValue == Decimal.Floor(decimalValue))
+                {
+                    entryFields[fieldId][locale] = Decimal.ToInt64(decimalValue);
+                    break;
+                }
+
+                entryFields[fieldId][locale] = decimalValue;
+
+                break;
+            case "Symbol" or "Text":
+                entryFields[fieldId][locale] = HttpUtility.HtmlDecode(htmlNode.InnerText);
+                break;
+            case "Object" or "Location":
+                var jsonValue = htmlNode.Attributes["data-contentful-json-value"].Value;
+                var jsonObject = JToken.Parse(HttpUtility.HtmlDecode(jsonValue));
+                entryFields[fieldId][locale] = jsonObject;
+                break;
+            case "Boolean":
+                var boolValue = Convert.ToBoolean(htmlNode.Attributes["data-contentful-bool-value"].Value);
+                entryFields[fieldId][locale] = boolValue;
+                break;
+            case "RichText":
+                var htmlToRichTextConverter = new HtmlToRichTextConverter();
+                var richText = htmlToRichTextConverter.ToRichText(htmlNode.InnerHtml);
+                var serializerSettings = new JsonSerializerSettings()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+                entryFields[fieldId][locale] =
+                    JObject.Parse(JsonConvert.SerializeObject(richText, serializerSettings));
+                break;
+            case "Link":
+                var linkType = htmlNode.Attributes["data-contentful-link-type"].Value;
+                var id = htmlNode.Attributes["data-contentful-link-id"].Value;
+                var localized = htmlNode.Attributes["data-contentful-localized"]?.Value;
+                var linkData = new
+                {
+                    sys = new
                     {
-                        entryFields[fieldId][locale] = Decimal.ToInt64(decimalValue);
-                        break;
+                        type = "Link",
+                        linkType,
+                        id
                     }
-
-                    entryFields[fieldId][locale] = decimalValue;
-
-                    break;
-                case "Symbol" or "Text":
-                    entryFields[fieldId][locale] = HttpUtility.HtmlDecode(htmlNode.InnerText);
-                    break;
-                case "Object" or "Location":
-                    var jsonValue = htmlNode.Attributes["data-contentful-json-value"].Value;
-                    var jsonObject = JToken.Parse(HttpUtility.HtmlDecode(jsonValue));
-                    entryFields[fieldId][locale] = jsonObject;
-                    break;
-                case "Boolean":
-                    var boolValue = Convert.ToBoolean(htmlNode.Attributes["data-contentful-bool-value"].Value);
-                    entryFields[fieldId][locale] = boolValue;
-                    break;
-                case "RichText":
-                    var htmlToRichTextConverter = new HtmlToRichTextConverter();
-                    var richText = htmlToRichTextConverter.ToRichText(htmlNode.InnerHtml);
-                    var serializerSettings = new JsonSerializerSettings()
+                };
+                if (linkType == "Asset")
+                {
+                    entryFields[fieldId]![locale] = JObject.Parse(JsonConvert.SerializeObject(linkData));
+                }
+                else
+                {
+                    if (localized != null && localized.Equals("true", StringComparison.OrdinalIgnoreCase))
                     {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                        NullValueHandling = NullValueHandling.Ignore
-                    };
-                    entryFields[fieldId][locale] =
-                        JObject.Parse(JsonConvert.SerializeObject(richText, serializerSettings));
-                    break;
-                case "Link":
-                    var linkType = htmlNode.Attributes["data-contentful-link-type"].Value;
-                    var id = htmlNode.Attributes["data-contentful-link-id"].Value;
-                    var localized = htmlNode.Attributes["data-contentful-localized"]?.Value;
-                    var linkData = new
+                        entryFields[fieldId]![locale] = JObject.Parse(JsonConvert.SerializeObject(linkData));
+                    }
+                    else
+                    {
+                        var fieldObject = entryFields[fieldId] as JObject;
+                        if (fieldObject != null)
+                        {
+                            fieldObject.Remove(locale);
+                        }
+                    }
+                }
+
+                break;
+            case "Array":
+                if (htmlNode.Attributes.Any(a => a.Name == "data-contentful-link-type"))
+                {
+                    linkType = htmlNode.Attributes["data-contentful-link-type"].Value;
+                    var itemIds = htmlNode.Attributes["data-contentful-link-ids"].Value.Split(",");
+                    var arrayLocalized = htmlNode.Attributes["data-contentful-localized"]?.Value;
+                    var arrayData = itemIds.Select(id => new
                     {
                         sys = new
                         {
@@ -97,86 +124,45 @@ public static class EntryToJsonConverter
                             linkType,
                             id
                         }
-                    };
-                    if (linkType == "Asset")
+                    });
+
+                    if (linkType == "Entry")
                     {
-                        entryFields[fieldId]![locale] = JObject.Parse(JsonConvert.SerializeObject(linkData));
-                    }
-                    else
-                    {
-                        if (localized != null && localized.Equals("true", StringComparison.OrdinalIgnoreCase))
+                        if (arrayLocalized != null &&
+                            arrayLocalized.Equals("true", StringComparison.OrdinalIgnoreCase))
                         {
-                            entryFields[fieldId]![locale] = JObject.Parse(JsonConvert.SerializeObject(linkData));
+                            entryFields[fieldId]![locale] =
+                                JArray.Parse(JsonConvert.SerializeObject(arrayData));
                         }
                         else
                         {
                             var fieldObject = entryFields[fieldId] as JObject;
                             if (fieldObject != null)
                             {
-                                fieldObject.Remove(locale);
-                            }
-                        }
-                    }
-
-                    break;
-                case "Array":
-                    if (htmlNode.Attributes.Any(a => a.Name == "data-contentful-link-type"))
-                    {
-                        linkType = htmlNode.Attributes["data-contentful-link-type"].Value;
-                        var itemIds = htmlNode.Attributes["data-contentful-link-ids"].Value.Split(",");
-                        var arrayLocalized = htmlNode.Attributes["data-contentful-localized"]?.Value;
-                        var arrayData = itemIds.Select(id => new
-                        {
-                            sys = new
-                            {
-                                type = "Link",
-                                linkType,
-                                id
-                            }
-                        });
-
-                        if (linkType == "Entry")
-                        {
-                            if (arrayLocalized != null &&
-                                arrayLocalized.Equals("true", StringComparison.OrdinalIgnoreCase))
-                            {
-                                entryFields[fieldId]![locale] =
-                                    JArray.Parse(JsonConvert.SerializeObject(arrayData));
-                            }
-                            else
-                            {
-                                var fieldObject = entryFields[fieldId] as JObject;
-                                if (fieldObject != null)
+                                if (fieldObject.ContainsKey(locale))
                                 {
-                                    if (fieldObject.ContainsKey(locale))
-                                    {
-                                        fieldObject.Remove(locale);
-                                    }
+                                    fieldObject.Remove(locale);
                                 }
                             }
-                        }
-                        else
-                        {
-                            entryFields[fieldId][locale] =
-                                JArray.Parse(JsonConvert.SerializeObject(arrayData));
                         }
                     }
                     else
                     {
-                        var arrayContent = htmlNode
-                            .Descendants()
-                            .Where(x => x.Name == HtmlConstants.Li)
-                            .Select(x => HttpUtility.HtmlDecode(x.InnerText));
-
-                        entryFields[fieldId][locale] = JArray.FromObject(arrayContent);
+                        entryFields[fieldId][locale] =
+                            JArray.Parse(JsonConvert.SerializeObject(arrayData));
                     }
+                }
+                else
+                {
+                    var arrayContent = htmlNode
+                        .Descendants()
+                        .Where(x => x.Name == HtmlConstants.Li)
+                        .Select(x => HttpUtility.HtmlDecode(x.InnerText));
 
-                    break;
-            }
-        }
-        catch (Exception e)
-        {
-            throw;
+                    entryFields[fieldId][locale] = JArray.FromObject(arrayContent);
+                }
+
+                break;
         }
     }
 }
