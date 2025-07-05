0af2bd68277bf018ebed1ee425c20d3b34466992
mathijs-bb <mathijs.sonnemans@blackbird.io>
2025-07-05 18:10:04 +0200

    Fixed order to upload entry

--- Apps.Contentful/Models/Requests/Tags/UploadEntryRequest.cs
+++ Apps.Contentful/Models/Requests/Tags/UploadEntryRequest.cs
@@ -4,23 +4,18 @@ using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Files;
 using Blackbird.Applications.SDK.Blueprints.Interfaces.CMS;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Apps.Contentful.Models.Requests.Tags;
 public class UploadEntryRequest : EnvironmentIdentifier, IUploadContentInput
 {
-    [Display("Entry ID")]
-    [DataSource(typeof(EntryDataSourceHandler))]
-    public string? ContentId { get; set; }
+    public FileReference Content { get; set; }
 
     [DataSource(typeof(LocaleDataSourceHandler))]
     public string Locale { get; set; }
 
-    public FileReference Content { get; set; }
+    [Display("Entry ID")]
+    [DataSource(typeof(EntryDataSourceHandler))]
+    public string? ContentId { get; set; }
 
     [Display("Don't update reference fields")]
     public bool? DontUpdateReferenceFields { get; set; }
