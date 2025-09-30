f061a6ec55b568e25e3643a3d7cf54786bd2783f
ce-nist <cecilia.nistal@blackbird.io>
2025-09-30 08:12:27 -0300

    Added Get entry's array field action

---
+++ Apps.Contentful/Models/Responses/GetArrayContentResponse.cs
@@ -0,0 +1,12 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Contentful.Models.Responses;
+
+public class GetArrayContentResponse
+{
+    [Display("Array content")]
+    public List<string>? ArrayContent { get; set; }
+
+    [Display("Missing locales")]
+    public List<string> MissingLocales { get; set; }
+}
