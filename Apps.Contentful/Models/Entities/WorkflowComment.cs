c814abbc09fc03d1b5a0c55e69ee5bde88bd455f
RiabushenkoA <ariabushenko@blackbird.io>
2025-10-06 19:57:55 +0300

    Added coment response in webhook 'On workflow updated'

---
+++ Apps.Contentful/Models/Entities/WorkflowComment.cs
@@ -0,0 +1,22 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Contentful.Models.Entities
+{
+    public class WorkflowComment
+    {
+        [Display("Comment ID")]
+        public string CommentId { get; set; } = string.Empty;
+
+        [Display("Body")]
+        public string Body { get; set; } = string.Empty;
+
+        [Display("Created at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Display("Updated at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Display("Created by (ID)")]
+        public string? CreatedById { get; set; }
+    }
+}
