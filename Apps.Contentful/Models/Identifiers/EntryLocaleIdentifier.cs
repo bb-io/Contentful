c7e095e254e9e030e292adc4c86ae95f6b25716f
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-24 10:42:08 +0300

    add EntryLocaleIdentifier.Validate and use this method in actions, add tests

--- Apps.Contentful/Models/Identifiers/EntryLocaleIdentifier.cs
+++ Apps.Contentful/Models/Identifiers/EntryLocaleIdentifier.cs
@@ -1,6 +1,7 @@
 using Apps.Contentful.DataSourceHandlers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 
 namespace Apps.Contentful.Models.Identifiers;
 
@@ -9,4 +10,12 @@ public class EntryLocaleIdentifier : LocaleIdentifier
     [Display("Entry ID")]
     [DataSource(typeof(EntryLocaleDataSourceHandler))]
     public string EntryId { get; set; }
+
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(EntryId))
+            throw new PluginMisconfigurationException("Entry ID must be provided. Please check your input and try again");
+        if (string.IsNullOrEmpty(Locale))
+            throw new PluginMisconfigurationException("Locale must be provided. Please check your input and try again");
+    }
 }
--- Tests.Contentful/EntryFieldActionsTests.cs
+++ Tests.Contentful/EntryFieldActionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Apps.Contentful.Actions;
 using Apps.Contentful.Models.Identifiers;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 using Newtonsoft.Json;
 using Tests.Contentful.Base;
 
@@ -26,5 +27,67 @@ public class EntryFieldActionsTests : TestBase
         
         Assert.IsNull(response);
         Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+    }
+
+    [TestMethod]
+    public async Task GetTextFieldContent_ValidRequest_ShouldReturnFieldValue()
+    {
+        // Arrange
+        var action = new EntryFieldActions(InvocationContext);
+        var entry = new EntryLocaleIdentifier 
+        { 
+            EntryId = "5746dLKTkEZjOQX21HX2KI", 
+            Locale = "de",
+        };
+        var field = new FieldIdentifier { FieldId = "title" };
+
+        // Act
+        var response = await action.GetTextFieldContent(entry, field);
+            
+        // Assert
+        Assert.IsNotNull(response);
+        Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+    }
+
+    [TestMethod]
+    public async Task GetTextFieldContent_WithNullLocale_ThrowsMisconfigException()
+    {
+        // Arrange
+        var action = new EntryFieldActions(InvocationContext);
+        var entry = new EntryLocaleIdentifier
+        {
+            EntryId = "5746dLKTkEZjOQX21HX2KI",
+            Locale = null,
+        };
+        var field = new FieldIdentifier { FieldId = "title" };
+
+        // Act
+        var ex = await Assert.ThrowsExceptionAsync<PluginMisconfigurationException>(
+            async () => await action.GetTextFieldContent(entry, field)
+        );
+
+        // Assert
+        StringAssert.Contains(ex.Message, "Locale must be provided. Please check your input and try again");
+    }
+
+    [TestMethod]
+    public async Task GetTextFieldContent_WithNullEntryId_ThrowsMisconfigException()
+    {
+        // Arrange
+        var action = new EntryFieldActions(InvocationContext);
+        var entry = new EntryLocaleIdentifier
+        {
+            EntryId = null,
+            Locale = "de",
+        };
+        var field = new FieldIdentifier { FieldId = "title" };
+
+        // Act
+        var ex = await Assert.ThrowsExceptionAsync<PluginMisconfigurationException>(
+            async () => await action.GetTextFieldContent(entry, field)
+        );
+
+        // Assert
+        StringAssert.Contains(ex.Message, "Entry ID must be provided. Please check your input and try again");
     }
 }
