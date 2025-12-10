b570deab9c7559e918aa182e55ef97bce39854bd
ce-nist <cecilia.nistal@blackbird.io>
2025-12-10 19:38:27 -0300

    Added filtering by content model and recursive search to Ger referenced entries action

--- Apps.Contentful/Models/Requests/GetReferenceEntriesRequest.cs
+++ Apps.Contentful/Models/Requests/GetReferenceEntriesRequest.cs
@@ -10,4 +10,10 @@ public class GetReferenceEntriesRequest : EntryIdentifier
     [Display("Field IDs", Description = "Specific reference field IDs to check. If not specified, all reference fields will be checked.")]
     [DataSource(typeof(ReferenceFieldDataHandler))]
     public IEnumerable<string>? FieldIds { get; set; }
+
+    [Display("Content type IDs", Description = "Specific content type model IDs to check. If not specified, all content type models will be checked.")]
+    public IEnumerable<string>? ContentTypeIds { get; set; }
+
+    [Display("Search recursively")]
+    public bool? SearchRecursively { get; set; } = false;
 }
